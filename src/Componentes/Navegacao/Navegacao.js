5574c708c93f0a3e4db99cf2d51a789c54132cba
Paulo <pauloh16.pdr@gmail.com>
2021-04-17 17:22:40 -0300

    Estilizar componente Navegacao

--- src/Componentes/Navegacao/Navegacao.js
+++ src/Componentes/Navegacao/Navegacao.js
@@ -7,27 +7,27 @@ const Navegacao = () => {
         <nav className={estilos.Navegacao}>
             <ul>
                 <li>
-                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/" activeClassName={estilos.ativo} end>Home</NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/regras">WCAG 2.0</NavLink>
+                    <NavLink to="/regras"  activeClassName={estilos.ativo}>WCAG 2.0</NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/testes">Testes</NavLink>
+                    <NavLink to="/testes"  activeClassName={estilos.ativo}>Testes</NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/dicas">Dicas</NavLink>
+                    <NavLink to="/dicas"  activeClassName={estilos.ativo}>Dicas</NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/tutorial">Tutorial</NavLink>
+                    <NavLink to="/tutorial"  activeClassName={estilos.ativo}>Tutorial</NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/desenvolvedores">Desenvolvedores</NavLink>
+                    <NavLink to="/desenvolvedores"  activeClassName={estilos.ativo}>Desenvolvedores</NavLink>
                 </li>
             </ul>
         </nav>
