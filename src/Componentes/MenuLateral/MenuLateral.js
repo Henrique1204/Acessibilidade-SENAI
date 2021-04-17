99c4012c92d9377b7a9cd050db31dc96cf926f54
Paulo <pauloh16.pdr@gmail.com>
2021-04-17 16:27:03 -0300

    Adicionar estilo geral do MenuLateral

--- src/Componentes/MenuLateral/MenuLateral.js
+++ src/Componentes/MenuLateral/MenuLateral.js
@@ -7,23 +7,27 @@ import Icone from '../Util/Icone';
 const MenuLateral = () => {
     return (
         <aside className={estilos.MenuLateral}>
-            <button>
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
+            <div className={estilos.conteudo}>
+                <button>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
 
-            <div>
-                <h2>Páginas do site</h2>
-                <Navegacao />
+                <div>
+                    <h2>Páginas do site</h2>
+                    <Navegacao />
+                </div>
+
+                <div className={estilos.acessibilidade}>
+                    <h1>Opções de acessibilidades</h1>
+                    <OpcoesAcessibilidade />
+                </div>
+
+                <div className={estilos.icone}>
+                    <Icone nome="cog" />
+                </div>
             </div>
-
-            <div>
-                <h1>Opções de acessibilidades</h1>
-                <OpcoesAcessibilidade />
-            </div>
-
-            <Icone nome="cog" />
         </aside>
     );
 };
