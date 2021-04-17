1c5bf67cebfbeb551cdea786ef2743d79a301ede
Paulo <pauloh16.pdr@gmail.com>
2021-04-17 15:08:53 -0300

    Estilizar componente Header

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -3,8 +3,11 @@ import estilos from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className={estilos.header}>
-            <h1>Header</h1>
+        <header className={estilos.Header}>
+            <div className="container">
+                <h3>Página essa aqui</h3>
+                <h1>Titulo da introdução da página</h1>
+            </div>
         </header>
     );
 };
