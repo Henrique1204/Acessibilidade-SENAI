ed55a876cdfeebf5dfc89c49ca6a42bc82cb2dfa
Paulo <pauloh16.pdr@gmail.com>
2021-04-17 19:09:09 -0300

    Implementar acoes de abrir e fechar menu

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import Footer from './Componentes/Footer/Footer';
 
 const App = () => {
   return (
-    <div className="App">
-      <MenuLateral />
-  
-      <div className="conteudo">
-        <Header />
-        <Rotas />
+    <>
+      <div className="containerGeral">
+        <MenuLateral />
+    
+        <div className="conteudo">
+          <Header />
+          <Rotas />
+        </div>
       </div>
 
       <Footer />
-    </div>
+    </>
   );
 };
 
--- src/Componentes/MenuLateral/MenuLateral.js
+++ src/Componentes/MenuLateral/MenuLateral.js
@@ -5,27 +5,37 @@ import OpcoesAcessibilidade from '../OpcoesAcessibilidade/OpcoesAcessibilidade';
 import Icone from '../Util/Icone';
 
 const MenuLateral = () => {
+    const [ativo, setAtivo] = React.useState(false);
+
     return (
-        <aside className={estilos.MenuLateral}>
-                <button className={estilos.hamburguer}>
+        <aside
+            className={`${estilos.MenuLateral} ${(ativo) ? estilos.aberto : estilos.fechado}`}
+        >
+                <button className={estilos.hamburguer} onClick={() => setAtivo((ativo) => !ativo)}>
                     <span></span>
                     <span></span>
                     <span></span>
                 </button>
 
-                <div>
-                    <h2>Páginas do site</h2>
-                    <Navegacao />
-                </div>
+                { ativo && (
+                    <>
+                        <div>
+                            <h2>Páginas do site</h2>
+                            <Navegacao />
+                        </div>
 
-                <div className={estilos.acessibilidade}>
-                    <h1>Opções de acessibilidades</h1>
-                    <OpcoesAcessibilidade />
-                </div>
+                        <div className={estilos.acessibilidade}>
+                            <h1>Opções de acessibilidades</h1>
+                            <OpcoesAcessibilidade />
+                        </div>
+                    </>
+                ) }
 
-                <button className={estilos.icone}>
-                    <Icone nome="cog" />
-                </button>
+                { !ativo  && (
+                    <button className={estilos.icone}>
+                        <Icone nome="cog" />
+                    </button>
+                ) }
         </aside>
     );
 };
