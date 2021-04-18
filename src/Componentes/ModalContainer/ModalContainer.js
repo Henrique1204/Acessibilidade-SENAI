79927eb4d7e2d8e627a5b9f8aafddb0ef7fe17ac
Paulo <pauloh16.pdr@gmail.com>
2021-04-18 20:07:39 -0300

    Adicionar componente para container dos modais

---
+++ src/Componentes/ModalContainer/ModalContainer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Icone from '../Util/Icone';
+import estilos from './ModalContainer.module.css';
+
+const ModalContainer = ({ setAtivo, children, classe }) => {
+    const fundoRef = React.useRef();
+  
+    const fecharModal = ({ target }) => {
+      if (target === fundoRef.current) setAtivo(false);
+    }
+  
+    React.useEffect(() => {
+      const fecharModalEsc = ({ key }) => {
+        if (key === 'Escape') setAtivo(false);
+      };
+
+      window.addEventListener('keydown', fecharModalEsc);
+
+      return () => {
+        window.removeEventListener('keydown', fecharModalEsc);
+      };
+    }, [setAtivo]);
+  
+    return (
+      <div className={estilos.Modal} onClick={fecharModal} ref={fundoRef}>
+        <div className={`${estilos.CardModal} ${(classe) ? classe : ''}`}>
+          <button className={estilos.ModalFechar} onClick={() => setAtivo(false)}>
+            <Icone tipo="fa" nome="times" />
+          </button>
+
+            { children }
+        </div>
+      </div>
+    );
+  };
+
+export default ModalContainer;
